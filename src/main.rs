be657a8ad8c0370ce91e8339ef6748533a626524
ekusiadadus <ekusiadadus@gmail.com>
2024-03-11 19:48:02 +0900

    feat: udpate run file

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,28 @@
+use std::{env, error::Error, fs, process::exit};
+
+fn run_file(path: &str) -> Result<(), Box<dyn Error>> {
+    let source = fs::read_to_string(path)?;
+
+    // run(source);
+    Ok(())
+}
+
+fn run_prompt() {
+    // loop {
+    //     let mut input = String::new();
+    //     std::io::stdin().read_line(&mut input).unwrap();
+    //     run(input);
+    // }
+}
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    if args.len() > 2 {
+        println!("Usage: jlox [script]");
+        exit(64);
+    } else if args.len() == 2 {
+        run_file(&args[2]);
+    } else {
+        run_prompt();
+    }
+    dbg!(args);
 }
