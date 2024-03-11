89098b5a9c9caf48fb8608eedefdb1c27ea1551e
ekusiadadus <ekusiadadus@gmail.com>
2024-03-11 20:25:38 +0900

    chore: chapter 1

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,38 @@
 use std::{env, error::Error, fs, process::exit};
 
-fn run_file(path: &str) -> Result<(), Box<dyn Error>> {
-    let source = fs::read_to_string(path)?;
-
-    // run(source);
-    Ok(())
+fn run(source: &str) -> Result<(), Box<dyn Error>> {
+    return Err("Not implemented".into());
 }
 
-fn run_prompt() {
-    // loop {
-    //     let mut input = String::new();
-    //     std::io::stdin().read_line(&mut input).unwrap();
-    //     run(input);
-    // }
+fn run_file(path: &str) -> Result<(), Box<dyn Error>> {
+    match fs::read_to_string(path) {
+        Ok(contents) => {
+            return run(&contents);
+        }
+        Err(e) => {
+            eprintln!("Error reading file: {}", e);
+            exit(74);
+        }
+    }
+}
+
+fn run_prompt() -> Result<(), Box<dyn Error>> {
+    loop {
+        let mut input = String::new();
+        match std::io::stdin().read_line(&mut input) {
+            Ok(_) => match run(&input) {
+                Ok(_) => {}
+                Err(e) => {
+                    eprintln!("Error running input: {}", e);
+                    exit(74);
+                }
+            },
+            Err(e) => {
+                eprintln!("Error reading input: {}", e);
+                exit(74);
+            }
+        }
+    }
 }
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -20,9 +40,21 @@ fn main() {
         println!("Usage: jlox [script]");
         exit(64);
     } else if args.len() == 2 {
-        run_file(&args[2]);
+        match run_file(&args[1]) {
+            Ok(_) => {}
+            Err(e) => {
+                eprintln!("Error running file: {}", e);
+                exit(74);
+            }
+        }
     } else {
-        run_prompt();
+        match run_prompt() {
+            Ok(_) => {}
+            Err(e) => {
+                eprintln!("Error running prompt: {}", e);
+                exit(74);
+            }
+        }
     }
     dbg!(args);
 }
